fa9dae6c43435cf5554d73911fd67e0a677267ac
Edbless <giftyhemans8@gmail.com>
2025-07-16 12:50:52 +0000

    second

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+// components/Navbar.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">Gifted</div>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/skills">Skills</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
